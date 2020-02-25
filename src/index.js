9ffd88ba73e417c86ce5efb2efaa7ebf33cb93a8
Mary Zhavnerovich <rostova0106@gmail.com>
2020-02-25 15:17:56 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let myArr = n.toString().split('');
+    let a = myArr.length;
+            if (myArr[0] === '-') {
+                myArr=myArr.slice(1);
+            } else if (myArr[a] === '0') {
+                myArr=myArr.slice(0, a-1);
+            } else {
+                myArr = myArr;
+            }
+    return Number(myArr.reverse().join('')); 
 }
